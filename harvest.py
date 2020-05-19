1ea25e7f479aaf8e9e7547c2cbc58eabb336935c
macbre <maciej.brencz@gmail.com>
2020-05-19 18:04:49 +0200

    Add upload_to_commons()

--- harvest.py
+++ harvest.py
@@ -1,13 +1,13 @@
 """
 List MBC sets
 """
-import json
 import logging
+from dataclasses import dataclass
 
 import pywikibot
 import requests
 
-from typing import Iterator
+from typing import Iterator, List
 
 from lxml.etree import ElementBase
 from sickle import Sickle, models, OAIResponse
@@ -15,6 +15,26 @@ from sickle import Sickle, models, OAIResponse
 DLIBRA_SERVER = 'http://mbc.cyfrowemazowsze.pl/'
 # DLIBRA_SERVER = 'https://www.wbc.poznan.pl/'
 OAI_ENDPOINT = f'{DLIBRA_SERVER}dlibra/oai-pmh-repository.xml'
+
+
+@dataclass
+class RecordMeta:
+    """
+    Encapsulates metadata of the record needed to upload a file to Wikimedia Commons
+    """
+    record_id: str  # e.g. oai:mbc.cyfrowemazowsze.pl:59990
+    source_id: str
+    title: str
+    date: str
+    content_url: str
+    tags: List[str]
+
+    @property
+    def record_numeric_id(self) -> int:
+        """
+        oai:mbc.cyfrowemazowsze.pl:59990 -> 59990
+        """
+        return int(str(self.record_id).split(':')[-1])
 
 
 def get_set(instance: Sickle, set_name: str) -> Iterator[models.Record]:
@@ -57,6 +77,46 @@ def get_content_url(record: models.Record) -> str:
     return url
 
 
+def upload_to_commons(record: RecordMeta):
+    """
+    Upload a given record to Commons
+    """
+    logger = logging.getLogger(name='upload_to_commons')
+
+    # format a file name
+    # https://commons.wikimedia.org/wiki/File:Portret_Leona_Wagenfisza_(73487).jpg
+    file_name = f'{record.title} ({record.record_numeric_id}).jpg'
+
+    file_desciption = """
+=={{int:filedesc}}==
+{{Artwork
+ |artist = 
+ |description = {{pl|%s}}
+ |date = %s
+ |medium =
+ |institution = {{Institution:Mazovian Digital Library}}
+ |notes =
+ |accession number = [http://fbc.pionier.net.pl/id/oai:mbc.cyfrowemazowsze.pl:%d oai:mbc.cyfrowemazowsze.pl:%d]
+ |source = 
+* http://mbc.cyfrowemazowsze.pl/dlibra/docmetadata?id=%d
+
+}}
+
+=={{int:license-header}}==
+{{PD-old-auto}}
+{{Mazovian Digital Library partnership}}
+
+[[Category:Media contributed by the Mazovian Digital Library (Ilustracja prasowa z XIX w.)]]
+
+[[Category:Uploaded with mbc-harvester]]
+    """.strip() % \
+        (record.title, record.date, record.record_numeric_id, record.record_numeric_id, record.record_numeric_id)
+
+    logger.info('Record metadata: %r', record)
+    logger.info('Uploading "%s" ...', file_name)
+    logger.info('File description: %s', file_desciption)
+
+
 def main():
     logger = logging.getLogger('oai-harvester')
     logger.info('Using <%s> OAI endpoint',  OAI_ENDPOINT)
@@ -65,20 +125,8 @@ def main():
 
     # https://www.mediawiki.org/wiki/Manual:Pywikibot/Create_your_own_script
     # https://doc.wikimedia.org/pywikibot/master/api_ref/index.html
-    #commons = pywikibot.Site()
-    #logger.info('pywikibot: %r', commons._siteinfo)
-
-    # print(harvester)
-    #
-    # for idx, set_ in enumerate(harvester.ListSets()):
-    #     logger.info('Set #%d found: %r', idx+1, set_)
-
-    # resource_id = 'oai:mbc.cyfrowemazowsze.pl:43271'
-
-    # https://www.wbc.poznan.pl/dlibra/oai-pmh-repository.xml?verb=GetRecord&metadataPrefix=oai_dc&identifier=oai:www.wbc.poznan.pl:168305
-    # <setSpec>MDL:CD:Warwilustrpras</setSpec>
-    # record = harvester.GetRecord(metadataPrefix='oai_dc', identifier=resource_id)
-    # print(record)
+    commons = pywikibot.Site()
+    logger.info('pywikibot: %r', commons._siteinfo)
 
     # https://www.wbc.poznan.pl/dlibra/oai-pmh-repository.xml?verb=ListRecords&metadataPrefix=oai_dc&set=rootCollection:wbc:ContemporaryRegionalMagazines
     set_name = 'MDL:CD:Warwilustrpras'
@@ -92,23 +140,25 @@ def main():
             # oai:mbc.cyfrowemazowsze.pl:59990 -> mbc.cyfrowemazowsze.pl
             source_id = str(record.header.identifier).split(':')[1]
 
-            record_meta = dict(
+            record_meta = RecordMeta(
                 record_id=record.header.identifier,
                 source_id=source_id,
                 title=record.metadata['title'][0],
+                date=str(record.metadata['date'][0]).strip('[]'),
                 content_url=get_content_url(record),
                 tags=sorted(list(set(record.metadata['subject']))),
             )
 
-            logger.info('Record metadata: %s', json.dumps(record_meta, indent=True))
+            # logger.info('Raw metadata: %r', record.metadata)
+            upload_to_commons(record=record_meta)
 
         except:
             logger.error('Exception', exc_info=True)
             pass
 
         # DEBUG
-        # if idx > 5:
-        #     break
+        if idx > 5:
+            break
 
 
 if __name__ == "__main__":
